b9116c71532497eafb3bcfe9a7ecd2f25fe77959
KyrieWangNYU <kw2116@nyu.edu>
2020-05-24 22:57:51 -0700

    Building a very simple API

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 // //Blocking synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -32,12 +33,35 @@ const http = require('http');
  * SERVER
  */
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-    res.end("Hello from the server!");
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview'){
+        res.end('This is OVERVIEW');
+    }else if(pathName === '/product'){
+        res.end('This is PRODUCT');
+    }else if(pathName === '/api'){
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        });
+        res.end(data);
+    }else{
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('PAGE NOT FOUND');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000');
 });
+
+
 
 
