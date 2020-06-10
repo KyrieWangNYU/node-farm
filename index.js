452ae32c20c97d7b9606485ba9fac9a17f762560
KyrieWangNYU <kw2116@nyu.edu>
2020-06-09 22:10:01 -0700

    installing prettier in VS Code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 
 const replaceTemplate = require('./modules/replaceTemplate');
 
@@ -35,62 +36,67 @@ const replaceTemplate = require('./modules/replaceTemplate');
  * SERVER
  */
 
-
-const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
-const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
-const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+
+console.log(slugs);
+
 const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
 
-    
-    const {query, pathname} = url.parse(req.url, true);
-   
+  //Overview Page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(templateCard, el))
+      .join('');
+    const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
-    //Overview Page
-    if (pathname === '/' || pathname === '/overview'){
-        res.writeHead(200, {
-            'Content-type': 'text/html'
-        });
-        const cardsHtml = dataObj.map(el => replaceTemplate(templateCard, el)).join('');
-        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-        
-        res.end(output);
-    }
-    //Product Page
-    else if(pathname === '/product'){
-        res.writeHead(200, {
-            'Content-type': 'text/html'
-        });
-        const product = dataObj[query.id];
-        const output = replaceTemplate(templateProduct, product);
-        res.end(output);
-    }
-    //API
-    else if(pathname === '/api'){
-        res.writeHead(200, {
-            'Content-type': 'application/json'
-        });
-        res.end(data);
-    }
-    //NOT FOUND
-    else{
-        res.writeHead(404, {
-            'Content-type': 'text/html',
-            'my-own-header': 'hello-world'
-        });
-        res.end('PAGE NOT FOUND');
-    }
+    res.end(output);
+  }
+  //Product Page
+  else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(templateProduct, product);
+    res.end(output);
+  }
+  //API
+  else if (pathname === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+    res.end(data);
+  }
+  //NOT FOUND
+  else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('PAGE NOT FOUND');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to requests on port 8000');
+  console.log('Listening to requests on port 8000');
 });
-
-
-
-
-
-
